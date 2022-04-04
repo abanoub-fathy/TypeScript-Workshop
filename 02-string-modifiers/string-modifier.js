b0f3874b9057bebf18aa3f881f7403cfa02c98b2
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-04-04 03:15:41 +0200

    Modify the compilation to be es6

--- 02-string-modifiers/string-modifier.js
+++ 02-string-modifiers/string-modifier.js
@@ -1,14 +1,12 @@
-var Strings = /** @class */ (function () {
-    function Strings() {
-    }
-    Strings.toPascalCase = function (txt) {
+"use strict";
+class Strings {
+    static toPascalCase(txt) {
         if (!txt.trim())
             return " ";
-        var wordsArr = txt.split(" ");
-        return wordsArr.map(function (word) { return word[0].toUpperCase() + word.toLowerCase().slice(1); }).join(" ");
-    };
-    return Strings;
-}());
+        const wordsArr = txt.split(" ");
+        return wordsArr.map(word => word[0].toUpperCase() + word.toLowerCase().slice(1)).join(" ");
+    }
+}
 console.log(Strings.toPascalCase("war AND peace")); // War And Peace
 console.log(Strings.toPascalCase("Catcher in the Rye")); // Catcher In The Rye
 console.log(Strings.toPascalCase("tO kILL A mOCKINGBIRD")); // To Kill A MockingBird
